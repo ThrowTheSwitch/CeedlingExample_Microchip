22830b6f57fb602506bb4dfaae9f010a6e491474
Vitor M. dos S. Alho <vitoralho1@gmail.com>
2021-01-11 22:37:23 -0300

    1 - "-Os" XC16 parameter was removed, now free versions of XC16 compiler can execute. 2 - One line removed inside "sim_test_fixture.rb" that change '\n' for ''. For some reason, this cause an error with the latest version of ceedling.

--- test/simulation/sim_test_fixture.rb
+++ test/simulation/sim_test_fixture.rb
@@ -4,6 +4,5 @@ IO.popen("sim30 test/simulation/sim_instructions.txt")
 sleep 1
 if File.exists? OUT_FILE 
     file_contents = File.read OUT_FILE
-    file_contents.gsub!("\n", "")
     print file_contents
 end
