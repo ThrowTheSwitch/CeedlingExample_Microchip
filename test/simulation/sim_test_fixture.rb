b631da4f838234ee1858a32e0aaebd439165a377
Paul Jacobson <20469645+pajacobson@users.noreply.github.com>
2022-12-04 18:04:58 +1100

    Update for mdb

--- test/simulation/sim_test_fixture.rb
+++ test/simulation/sim_test_fixture.rb
@@ -1,8 +1,35 @@
-OUT_FILE = "test/simulation/out.txt"
-File.delete OUT_FILE if File.exists? OUT_FILE 
-IO.popen("sim30 test/simulation/sim_instructions.txt")
-sleep 1
-if File.exists? OUT_FILE 
+# OUT_FILE = "test/simulation/out.txt"
+# File.delete OUT_FILE if File.exists? OUT_FILE
+# IO.popen("sim30 test/simulation/sim_instructions.txt")
+# sleep 1
+# if File.exists? OUT_FILE
+#     file_contents = File.read OUT_FILE
+#     print file_contents
+# end
+
+require 'rbconfig'
+
+OUT_FILE = "./test/simulation/out.txt"
+
+File.delete OUT_FILE if File.exists? OUT_FILE
+
+# this_os = RbConfig::CONFIG['host_os']
+
+# if (this_os =~ /mswin|mingw|cygwin/)
+    # var = IO.popen("c:\\Program Files (x86)\\Microchip\\MPLABX\\mplab_ide\\bin>mdb.bat ./test/simulation/sim_instructions.txt > " + OUT_FILE)
+# elsif (this_os =~ /darwin1[[:digit:]]/)
+var = IO.popen("/Applications/microchip/mplabx/v6.05/mplab_platform/bin/mdb.sh  ./test/simulation/sim_instructions.txt")
+    # var = "/Applications/microchip/mplabx/v6.05/mplab_platform/bin/mdb.sh  ./test/simulation/sim_instructions.txt"
+# end
+
+result = var.readlines
+Process.waitall
+
+
+if File.exists? OUT_FILE
     file_contents = File.read OUT_FILE
     print file_contents
+else
+    print result
+
 end
